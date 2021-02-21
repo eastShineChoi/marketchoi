57f66006d0281e7ce01fe82395cb634c6112890a
최동호 <eastshine@choedonghoui-MacBookPro.local>
2021-02-21 22:03:36 +0900

    marketchoi 프로젝트생성

---
+++ marketchoi-api/src/main/java/kr/co/eastshine/marketchoi/MarketchoiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.eastshine.marketchoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketchoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketchoiApplication.class, args);
+	}
+
+}
---
+++ marketchoi-api/src/test/java/kr/co/eastshine/marketchoi/MarketchoiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.eastshine.marketchoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketchoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
